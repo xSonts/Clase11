6bcc9b30857890fef3866567f7bed7b339e85ca4
xSonts <goasss24@gmail.com>
2024-10-17 18:02:32 -0500

    se agrego estilos

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="mi-primer-pwa-proptypes logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>mi-primer-pwa-proptypes</h1>
+      <h1>Calculadora</h1>
       <div className="card">
         <Calculadora/>
       </div>
